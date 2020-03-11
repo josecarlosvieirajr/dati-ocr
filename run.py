73e78efb229656a8e2527eec5aaceaf0411a6ec6
JoseCarlosVJr <josejunior.vieira@hotmail.com>
2020-03-11 20:48:49 -0300

    name from functions

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 from flask import Flask, request, jsonify
 
+from helpers.exception_ocr import exception_handler
 from services.api_dati_ocr import WebApiOcr, WebApiTrain
 
 app = Flask(__name__)
 
 
 @app.route('/send_ocr_file', methods=['POST'])
-def first():
+def convert_file_to_text():
     content = WebApiOcr().run(request.files['file'])
     return jsonify({'status': 'success', 'content': content})
 
 
 @app.route('/send_data_selected', methods=['POST'])
-def first():
+def train_machine_learning():
     content = WebApiTrain().run(request.args)
     return jsonify({'status': 'success', 'content': content})
 
