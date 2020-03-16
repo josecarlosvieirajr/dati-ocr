8df216186ab9c7bd75e0041a7d3726254e712c5a
JoseCarlosVJr <josejunior.vieira@hotmail.com>
2020-03-16 09:51:41 -0300

    ajuste erro de cors

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from services.api_dati_ocr import WebApiOcr, WebApiTrain
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/": {"origins": "*"}})
 
 
 @app.route('/', methods=['GET'])
