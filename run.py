83d88db7382cadbdd595ab397214c54ccf7eb3f0
Jose Carlos Jr <jose.sysadm@gmail.com>
2020-04-07 13:01:39 -0300

    init gunicorn

--- run.py
+++ run.py
@@ -10,6 +10,3 @@ app.add_url_rule('/', methods=['GET'], view_func=view.home)
 app.add_url_rule('/send_ocr_file', methods=['POST'], view_func=view.convert_file_to_text)
 app.add_url_rule('/send_data_selected', methods=['POST'], view_func=view.train_machine_learning)
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, threaded=False, debug=True)
